f7d116c083352b5611d208d1b41a307d3c22fcdd
Matt West <matt@mattdanger.net>
2015-12-28 12:13:40 -0500

    Add PhoneValidator.

---
+++ Validator/PhoneValidator.php
@@ -0,0 +1,50 @@
+<?php
+namespace PhalconUtils\Validator;
+
+use Phalcon\Validation\Validator;
+use Phalcon\Validation\ValidatorInterface;
+use Phalcon\Validation\Message;
+use Phalcon\DI;
+
+class PhoneValidator extends Validator implements ValidatorInterface
+{
+
+  public function __construct($options = null)
+  {
+
+    parent::__construct($options);
+    $this->setOption("cancelOnFail", TRUE);
+
+  }
+
+
+  public function validate($validator, $attribute)
+  {
+    //obtain value
+    $value = $validator->getValue($attribute);
+
+    // Source https://ericholmes.ca/php-phone-number-validation-revisited/
+    $regex = "/^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/i";
+
+    if (preg_match($regex, $value)) {
+
+      // If phone number is valid
+      return TRUE;
+
+    }
+
+    // If value is not valid, set error
+    $message = $this->getOption('message');
+    if (!$message) {
+
+      // Set default error
+      $message = 'Phone number must be in the format: (555) 555-5555';
+
+    }
+
+    $validator->appendMessage(new Message($message, $attribute, 'Phone'));
+
+    return FALSE;
+  }
+
+}
